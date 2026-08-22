72a9a185457a1fee6eaeb00ead19ef17020652ca
MiguelForcaMariani <miguelforcamariani@gmail.com>
2026-08-21 23:37:21 -0300

    vai se fuder meia noite nessa porra

--- Conj.m
+++ Conj.m
@@ -1,26 +1,26 @@
 function [A, B] = Conj(A, B)
-  fprintf("Digite os valores do conjunto A\nQuando uma letra for inserida, a insercao sera interrompida");
+  fprintf("\nQuando uma letra for inserida, a insercao sera interrompida");
   A = [];
   B = [];
   a = 0;
   cont = 1;
   do
-    fprintf("Digite o %d elemento", cont);
+    fprintf("\nDigite o %d elemento do conjunto A: ", cont);
     a = input("", "s");
     a = str2double(a);
     cont++;
-    if isnan(a)
+    if !isnan(a)
       A = [A, a];
     endif
   until isnan(a);
   b = 0;
   cont = 1;
   do
-    fprintf("Digite o %d elemento", cont);
+    fprintf("\nDigite o %d elemento do conjunto B: ", cont);
     b = input("", "s");
     b = str2double(b);
     cont++;
-    if isnan(b)
+    if !isnan(b)
       B = [B, b];
     endif
   until isnan(b);
--- Main.m
+++ Main.m
@@ -8,7 +8,7 @@ do
       o = input("\nDigite a opcao selecionada: ", "s");
       o = str2double(o);
     if (isnan(o))
-      fprintf("\nA opcao digitada nao e um numero\n Digite novamente\n");
+      fprintf("\nA opcao digitada nao e um numero\nDigite novamente\n");
     endif
   until(!isnan(o));
       switch (o)
@@ -16,24 +16,36 @@ do
           [A, B] = Conj(A, B);
         case 2
           d = Uni(A, B);
+          fprintf("\n|--------resultado--------|");
           disp(d);
+          fprintf("\n|-------------------------|");
         case 3
           d = Inter(A, B);
+          fprintf("\n|--------resultado--------|");
           disp(d);
+          fprintf("\n|-------------------------|");
         case 4
           d = DifAB(A, B);
+          fprintf("\n|--------resultado--------|");
           disp(d);
+          fprintf("\n|-------------------------|");
         case 5
           d = DifBA(A, B);
+          fprintf("\n|--------resultado--------|");
           disp(d);
+          fprintf("\n|-------------------------|");
         case 6
           d = DifSim(A, B);
+          fprintf("\n|--------resultado--------|");
           disp(d);
+          fprintf("\n|-------------------------|");
         case 7
           d = ProdCart(A, B);
+          fprintf("\n|--------resultado--------|");
           disp(d);
+          fprintf("\n|-------------------------|");
         case 0
         otherwise
           fprintf("\nValor invalido, digite novamente\n");
       endswitch
-until (o != 0);
+until (o == 0);
